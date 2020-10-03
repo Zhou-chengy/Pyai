152aab5ed2b0d87357d5756bfda6c48b310fbbab
Zhou-chengy <70565781+Zhou-chengy@users.noreply.github.com>
2020-10-03 15:51:02 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,64 @@
+__version__ = '1.0.1'
+
+class robot(object):
+  def myrobot(self):
+    print('你好，请问需要什么帮助？')
+    ji = input()
+    if ji=='你好':
+      print('你好')
+
+    if ji=='你的名字叫什么':
+      print('我的名字叫Robin')
+
+    if ji=='你会说中文吗':
+      print('我当然会啊！')
+
+    if ji=='你会Pyhon吗？':
+      print('我想做一条蟒蛇，把你紧紧缠住，一辈子')
+
+    if ji=='你会说中文吗？':
+      print('我说的都是标准中文')
+
+    if ji=='圆周率':
+      print('3.14159265358979')
+
+    if ji=='你会编程吗？':
+      print('当然会啊，有我不会东西吗？哈哈哈')
+
+    if ji=='会Java吗？':
+      print('编程这东西主要在于你的思想，而不是什么语言')
+
+    if ji=='你很会说话':
+      print('我嘴刚抹了蜜')
+
+    if ji=='谢谢':
+      print('不客气，应该的')
+
+    if ji=='你会说什么语言':
+      print('我会中文')
+
+    if ji=='诸葛亮是谁':
+      print('   诸葛亮（181年~234年10月8日 [1]  ），字孔明，号卧龙，琅琊阳都（今山东省沂南县）人 [2]  ，三国时期蜀汉丞相，中国古')
+      print('代杰出的政治家、军事家、文学家。')
+
+    if ji=='中华人民共和国哪一年成立？':
+      print('1949年')
+      
+    if ji=='1+1等于几':
+      print('1+1=2')
+
+    if ji=='再见':
+      print('不想说拜拜，好像在和你聊一会儿啊！')
+      
+  def close(self):
+    break
+def main():
+  pass
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+ 
